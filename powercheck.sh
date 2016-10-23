03496954f912548ab86acccac04321e0cc37d2e1
Mitchell Zubarev-Foxworth <mitch.foxworth@gmail.com>
2016-10-22 23:48:40 -0400

    Create powercheck.sh

---
+++ powercheck.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+# Runs every 5 minutes via cron
+# Under Ubuntu 16.04; uses the "powerpanel" package
+# Checks the status output for "utility voltage" from a UPS attached via USB
+# If the utility voltage is 0, it connects via ssh to the vSphere host vhost1 and shuts it down
+
+export UTILITY_VOLTAGE=$(/usr/sbin/pwrstat -status | grep 'Utility Voltage' | awk {'print $3'})
+
+if [ $UTILITY_VOLTAGE == '0' ]; then
+        /usr/bin/ssh -i /home/mitchf/.ssh/id_rsa root@vhost1 '/sbin/shutdown.sh; /sbin/poweroff';
+        echo 'Shutting down vhost1' >> /var/log/pwrstatd.log
+fi
